69f0effb3ed2be3e2c0d5e2cacef5c658bbb5818
Jaap Badlands <jaap@deadlysyntax.com>
2015-05-05 15:22:17 +1000

    the age

---
+++ projects/theage/auxiliary.rb
@@ -0,0 +1,28 @@
+class Theage < Project
+	def initialize
+		project_name :theage
+	end
+	
+	
+	def crawl options
+    OutriderTools::Crawl::site( @config, ->(page, uri){
+      unless(  page.css('h1.cN-headingPage').text.strip.empty? )
+        clean_date = DateTime.strptime(page.css('.dtstamp time').text.strip, '%a %b %d %H:%M:%S %Z %Y').to_s #Tue Mar 03 08:27:23 UTC 2015
+        return {
+          :title_raw                 => page.css('h1.cN-headingPage').text.strip,
+          :author                    => page.css('.authorName a').text.strip,
+          :content_raw               => page.css('.articleBody p').map{ |paragraph| paragraph.text.strip }.to_json,
+          :date_published_raw        => page.css('.dtstamp time').text.strip,
+          :date_published_timestamp  => clean_date,
+          :status                    => 'scraped'
+        }
+      else
+        return {
+          :status              => 'rejected'
+        }
+      end
+    })
+  end
+  
+  
+end
