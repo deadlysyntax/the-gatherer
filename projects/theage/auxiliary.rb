3b48fcb324e103bdac3334cac7d3e7eb41805216
Jaap Badlands <jaap@deadlysyntax.com>
2015-05-17 16:55:38 +1000

    fix theage empty bug

--- projects/theage/auxiliary.rb
+++ projects/theage/auxiliary.rb
@@ -9,7 +9,7 @@ class Theage < Project
       
       unless(  page.css('.cN-headingPage').text.strip.empty?)
         clean_date = ''
-        clean_date = DateTime.strptime(page.css('.dtstamp time').text.strip, '%B %d, %Y').to_s unless page.css('.dtstamp time').text.strip.empty#Tue Mar 03 08:27:23 UTC 2015
+        clean_date = DateTime.strptime(page.css('.dtstamp time').text.strip, '%B %d, %Y').to_s unless page.css('.dtstamp time').text.strip.empty?#Tue Mar 03 08:27:23 UTC 2015
         return {
           :title_raw                 => page.css('h1.cN-headingPage').text.strip,
           :author                    => page.css('.authorName a').text.strip,
