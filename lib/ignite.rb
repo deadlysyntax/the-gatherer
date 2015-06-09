822988af594e1ec677ea3a56df5fc27f37870d7c
Jaap Badlands <jaap@deadlysyntax.com>
2015-06-09 21:44:48 +1000

    fix ruby version mistake

---
+++ lib/ignite.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+
+# Outrider loads in everything we need
+require_relative './outrider.rb'
+# The engine is initialized - this sets everything up for us
+engine   = Engine.new
+# The process of interprating commands is started
+engine.run
