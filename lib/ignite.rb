1ba53f3ba316250bd86c31ebcab3c6951cb63a56
Jaap Badlands <jaap@deadlysyntax.com>
2015-06-09 21:13:18 +1000

    ungit ignite.rb because the difference in ruby version on server

--- lib/ignite.rb
+++
@@ -1,8 +0,0 @@
-#!/Users/jaap/.rvm/rubies/ruby-2.2.1/bin/ruby
-
-# Outrider loads in everything we need
-require_relative './outrider.rb'
-# The engine is initialized - this sets everything up for us
-engine   = Engine.new
-# The process of interprating commands is started
-engine.run
